472222970dfdc0efd85520451254aa53f5f252cd
hotae <hotae319@gmail.com>
2019-12-16 02:47:25 -0800

    191216_0247

--- MPC_lanechange.m
+++ MPC_lanechange.m
@@ -12,8 +12,8 @@ z0 = [(x2+x3)/2;0;10;0];
 nz = 4; nu = 2;
 % Constraints
 
-d_safe = 5;       % distance to avoid collision
-vbar = 0; vlim = 50; % Limits
+d_safe = 7;       % distance to avoid collision
+vbar = 0; vlim = 30; % Limits
 alim = 10;           % acc limit
 betalim = 1;      % steering angle limit
 
@@ -49,8 +49,8 @@ feas = false([1,M]);
 % Target vehicle history
 % ztar = zeros(nz, M+1);
 ztar = zeros(nz*2, M+1); % target 2
-ztar(1:nz,1) = [x_goal;0;9;0];
-ztar(nz+1:end,1) = [x_goal;4;10;0];
+ztar(1:nz,1) = [x_goal;0;10.5;0];
+ztar(nz+1:end,1) = [x_goal;6;11;0];
 
 % MPC solve with measurement of target vehicle's state
 for t = 1:M
